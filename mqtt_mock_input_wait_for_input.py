027d9834aba6ddedcd78ab45716ce1d830c5c3fa
prgrobots <91106994+prgrobots@users.noreply.github.com>
2024-04-18 06:33:46 +0800

    updated

--- mqtt_mock_input_wait_for_input.py
+++ mqtt_mock_input_wait_for_input.py
@@ -1,8 +1,8 @@
 import paho.mqtt.client as mqtt
 import time
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+def on_connect(client, userdata, flags, reason_code, properties):
+    print("Connected with result code "+str(reason_code))
     client.subscribe("test/topic")
 
 def on_message(client, userdata, msg):
@@ -13,32 +13,27 @@ def wait_for_user_input():
     Waits for user input and returns the input value.
 
     Returns:
-        int: The user input value.
+        str: The user input value.
     """
-    try:
-        # TO DO
+# TO DO - Create a function to prompt the user for an int with error handling 
 
-    
-            
+       
 
-def main():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
 
-    client.connect("broker.example.com", 1883, 60)
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
 
-    while True:
-        client.loop()
 
-        # Wait for user input
-        user_input = wait_for_user_input()
-        
-        # If user input is valid, publish it to the MQTT topic
-        if user_input is not None:
-            client.publish("test/topic", user_input)
-            print("Published: " + user_input)
-            break  # Exit the loop after publishing the input value once
 
-if __name__ == "__main__":
-    main()
+# Wait for user input
+user_input = wait_for_user_input()
+
+# If user input is valid, publish it to the MQTT topic
+if user_input is not None:
+    mqttc.publish("test/topic", user_input)
+    print("Published: " + user_input)
+  
+
+mqttc.loop()
